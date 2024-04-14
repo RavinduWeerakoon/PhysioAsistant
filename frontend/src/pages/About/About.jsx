16ea606fd284cca037c8fa4019975676a92e3a6f
Ravindu Weerakoon <81863859+RavinduWeerakoon@users.noreply.github.com>
2024-04-14 19:24:08 +0000

    adding the new frontend

---
+++ frontend/src/pages/About/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './About.css'
+
+export default function About() {
+    return (
+        <div className="about-container">
+            <h1 className="about-heading">About</h1>
+            <div className="about-main">
+    
+            </div>
+        </div>
+    )
+}
